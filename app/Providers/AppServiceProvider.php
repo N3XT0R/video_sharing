505172d8c3caa428e333d408619ed2f4223fd0ae
Ilya Beliaev <info@php-dev.info>
2025-08-08 15:34:12 +0200

    added jobs.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
-use Spatie\Dropbox\Client;
+use Spatie\Dropbox\Client as DropboxClient;
 use Spatie\FlysystemDropbox\DropboxAdapter;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,8 +24,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Storage::extend('dropbox', function ($app, $config) {
-            $client = new Client($config['authorization_token']);
-            $adapter = new DropboxAdapter($client);
+            $client = new DropboxClient($config['authorization_token']);
+            $root = $config['root'] ?? '';
+            $adapter = new DropboxAdapter($client, $root);
+
             return new Filesystem($adapter);
         });
     }
