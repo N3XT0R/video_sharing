1776389c42df8d934ab79f57ea2a890a97d9d005
Ilya Beliaev <info@php-dev.info>
2025-08-11 19:56:30 +0200

    .

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,6 +4,11 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
+
+const csrf = document
+    .querySelector('meta[name="csrf-token"]')
+    ?.getAttribute('content');
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
@@ -12,6 +17,27 @@ window.Echo = new Echo({
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
+    wsPath: import.meta.env.VITE_REVERB_SERVER, // z.B. "/reverb/app"
 
-    wsPath: '/reverb',
+    authorizer: (channel) => ({
+        authorize: (socketId, callback) => {
+            fetch('/broadcasting/auth', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': csrf ?? '',
+                },
+                credentials: 'include',
+                body: JSON.stringify({
+                    socket_id: socketId,
+                    channel_name: channel.name,
+                }),
+            })
+                .then(async (res) =>
+                    res.ok ? callback(null, await res.json())
+                        : callback(new Error('Auth failed: ' + res.status), null)
+                )
+                .catch((err) => callback(err, null));
+        },
+    }),
 });
