c14633d6d00bafe6f68e0cf10918b7d76235835e
Ilya Beliaev <info@php-dev.info>
2025-08-11 20:44:47 +0200

    .

--- app/Services/PageService.php
+++ app/Services/PageService.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Models\Page;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Str;
 
 class PageService
 {
-    public function getHtml(string $section): ?string
+    public function getHtml(string $slug): ?string
     {
-        $page = Page::query()->where('section', $section)->first();
+        $page = Page::query()->where('slug', $slug)->first();
 
         return $page ? Str::markdown($page->content) : null;
+    }
+
+    public function getPagesForSection(string $section): Collection
+    {
+        return Page::query()->where('section', $section)->get();
     }
 }
