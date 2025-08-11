f05b0f16c84d46f0da36c3342ab2e426664b7b18
Ilya Beliaev <info@php-dev.info>
2025-08-12 01:05:00 +0200

    fix.

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return true; // oder deine Bedingung
+        return true;
     }
 }
