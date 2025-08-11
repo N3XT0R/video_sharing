0596b5a65c79388a0cf6bc5da50c377d8de662a5
Ilya Beliaev <N3XT0R@users.noreply.github.com>
2025-08-11 14:50:23 +0200

    Refactor ZipDownloader to separate download call

--- resources/js/components/ZipDownloader.js
+++ resources/js/components/ZipDownloader.js
@@ -65,18 +65,30 @@ export default class ZipDownloader {
             }
         );
 
-        const VERY_HIGH_TIMEOUT = 2147483647; // ~24 days
-        setTimeout(async () => {
+        let downloading = false;
+        const poll = setInterval(async () => {
             const { data: r } = await axios.get(`/zips/${jobId}/progress`);
             this.modal.update(r.progress || 0, r.status);
-            if (r.status === 'ready') {
-                const link = document.createElement('a');
-                link.href = `/zips/${jobId}/download`;
-                document.body.appendChild(link);
-                link.click();
-                link.remove();
-                this.modal.showClose();
+            if (r.status === 'ready' && !downloading) {
+                downloading = true;
+                clearInterval(poll);
+                await this.downloadZip(jobId);
             }
-        }, VERY_HIGH_TIMEOUT);
+        }, 500);
+    }
+
+    async downloadZip(jobId) {
+        const response = await axios.get(`/zips/${jobId}/download`, {
+            responseType: 'blob'
+        });
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', '');
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+        window.URL.revokeObjectURL(url);
+        this.modal.showClose();
     }
 }
