c2df333b469a19453c3411e89a9f3a8864fe2494
Ilya Beliaev <info@php-dev.info>
2025-08-13 14:36:54 +0200

    some refactorings.

---
+++ database/migrations/2025_08_13_142221_add_default_config_entries.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+
+    protected function getConfigEntries(): array
+    {
+        return [
+            [
+                'key' => 'email_admin_mail',
+                'value' => 'info@example.tld',
+                'cast_type' => 'string',
+                'is_visible' => 1,
+            ],
+            [
+                'key' => 'download_ttl_hours',
+                'value' => '144',
+                'cast_type' => 'int',
+                'is_visible' => 1,
+            ],
+            [
+                'key' => 'assign_expire_cooldown_days',
+                'value' => '14',
+                'cast_type' => 'int',
+                'is_visible' => 1,
+            ],
+        ];
+    }
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $entries = $this->getConfigEntries();
+        foreach ($entries as $entry) {
+            DB::table('configs')
+                ->insert($entry);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $entries = $this->getConfigEntries();
+        foreach ($entries as $entry) {
+            DB::table('configs')
+                ->where('key', $entry['key'])
+                ->delete();
+        }
+    }
+};
