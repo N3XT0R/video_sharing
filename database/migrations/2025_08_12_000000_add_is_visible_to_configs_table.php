ff533910bca0572ce506528ce920affbaa73f388
Ilya Beliaev <info@php-dev.info>
2025-08-13 12:57:36 +0200

    marked dropbox_refresh_token as not visible.

--- database/migrations/2025_08_12_000000_add_is_visible_to_configs_table.php
+++ database/migrations/2025_08_12_000000_add_is_visible_to_configs_table.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Models\Config;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,6 +13,15 @@ return new class extends Migration {
         Schema::table('configs', function (Blueprint $table) {
             $table->boolean('is_visible')->default(true);
         });
+
+        $config = Config::query()->where('key', 'dropbox_refresh_token')->firstOrNew();
+
+        if (!$config->exists) {
+            $config->setAttribute('key', 'dropbox_refresh_token');
+            $config->setAttribute('value', '');
+        }
+        $config->setAttribute('is_visible', false);
+        $config->save();
     }
 
     public function down(): void
