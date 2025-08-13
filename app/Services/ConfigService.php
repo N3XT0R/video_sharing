84ea172916cb42da4e0a25efdcbcdb02b471a9ea
Ilya Beliaev <info@php-dev.info>
2025-08-13 13:10:07 +0200

    some refactorings.

---
+++ app/Services/ConfigService.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Models\Config;
+use App\Services\Contracts\ConfigServiceInterface;
+
+class ConfigService implements ConfigServiceInterface
+{
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return rescue(
+            fn() => Config::query()->where('key', $key)->value('value') ?? $default,
+            $default
+        );
+    }
+}
---
+++ app/Services/Contracts/ConfigServiceInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Contracts;
+
+interface ConfigServiceInterface
+{
+    public function get(string $key, mixed $default = null): mixed;
+}
