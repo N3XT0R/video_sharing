ca3bb5f0a9bb8add1a47303ecc29778a0ab52d13
Ilya Beliaev <info@php-dev.info>
2025-08-14 13:20:32 +0200

    added new ScheduleConfigProvider.

--- app/Providers/ScheduleConfigProvider.php
+++ app/Providers/ScheduleConfigProvider.php
@@ -8,16 +8,10 @@ use App\Services\Schedule\ScheduleConfigFactory;
 use App\Services\Schedule\ScheduleConfigFactoryInterface;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Contracts\Support\DeferrableProvider;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class ScheduleConfigProvider extends ServiceProvider implements DeferrableProvider
 {
-    private const REQUIRED_TABLES = [
-        'configs',
-        'config_categories',
-        'config_sub_settings',
-    ];
 
     public function register(): void
     {
@@ -27,49 +21,16 @@ class ScheduleConfigProvider extends ServiceProvider implements DeferrableProvid
 
     public function boot(): void
     {
-        if (!$this->app->runningInConsole() || $this->isComposerPostCmd() || !$this->hasRequiredTables()) {
-            return;
+        try {
+            $this->app->afterResolving(Schedule::class, function (Schedule $schedule) {
+                $this->app->make(ScheduleConfigFactoryInterface::class)->register($schedule);
+            });
+        } catch (\Throwable $e) {
         }
-        $this->app->afterResolving(Schedule::class, function (Schedule $schedule) {
-            $this->app->make(ScheduleConfigFactoryInterface::class)->register($schedule);
-        });
     }
 
     public function provides(): array
     {
         return [ScheduleConfigFactoryInterface::class];
-    }
-
-    protected function hasRequiredTables(): bool
-    {
-        $result = true;
-        foreach (self::REQUIRED_TABLES as $table) {
-            $tmpResult = Schema::hasTable($table);
-            if ($tmpResult === false) {
-                $result = false;
-                break;
-            }
-        }
-        return $result;
-    }
-
-    private function isComposerPostCmd(): bool
-    {
-        if (!app()->runningInConsole()) {
-            return false;
-        }
-        $isComposer = getenv('COMPOSER_BINARY') !== false
-            || str_contains($_SERVER['argv'][0] ?? '', 'composer');
-
-        $laravelPostCmds = [
-            'package:discover',
-            'config:cache',
-            'event:cache',
-            'route:cache',
-            'view:cache',
-        ];
-        $isKnownArtisanPostCmd = in_array($_SERVER['argv'][1] ?? '', $laravelPostCmds, true);
-
-        return $isComposer && $isKnownArtisanPostCmd;
     }
 }
