6bf6c2849da91b2f29a234ac848dbfdfad995324
Ilya Beliaev <info@php-dev.info>
2025-08-18 00:56:04 +0200

    added new ScheduleConfigProvider.

--- app/Providers/ScheduleConfigProvider.php
+++ app/Providers/ScheduleConfigProvider.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Services\ConfigService;
 use App\Services\Contracts\ConfigRepositoryInterface;
+use App\Services\Contracts\ConfigServiceInterface;
 use App\Services\Repository\EloquentConfigRepository;
 use App\Services\Schedule\ScheduleConfigFactory;
 use App\Services\Schedule\ScheduleConfigFactoryInterface;
+use Illuminate\Cache\NullStore;
+use Illuminate\Cache\Repository;
 use Illuminate\Console\Events\CommandStarting;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Container\Container as Application;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
@@ -28,6 +33,18 @@ class ScheduleConfigProvider extends ServiceProvider implements DeferrableProvid
     {
         parent::register();
         $this->app->bind(ConfigRepositoryInterface::class, EloquentConfigRepository::class);
+        $this->app->bind(ConfigServiceInterface::class, static function (Application $app) {
+            /**
+             * @var Repository $cache
+             */
+            try {
+                $cache = $app['cache']->store();
+            } catch (\Throwable $e) {
+                $cache = new Repository(new NullStore());
+            }
+            
+            return new ConfigService($cache, $app->get(ConfigRepositoryInterface::class));
+        });
         $this->app->singleton(ScheduleConfigFactoryInterface::class, ScheduleConfigFactory::class);
     }
 
