76386082ec7a9510af64dfd8e31aa861749f25d2
Ilya Beliaev <info@php-dev.info>
2025-08-18 23:33:30 +0200

    fix.

--- lang/de/configs.php
+++ lang/de/configs.php
@@ -10,7 +10,6 @@ return [
     'keys' => [
         'email_admin_mail' => 'Admin-Mail-Adresse',
         'email_your_name' => 'Dein angezeigter Name',
-        'download_ttl_hours' => 'Download-Link Gültigkeit in Std.',
         'expire_after_days' => 'Assignment Gültigkeit in Tagen',
         'assign_expire_cooldown_days' => 'Cooldown-Tage je (channel, video)',
         'ingest_inbox_absolute_path' => 'Inbox-Pfad für Videos (absolut)',
