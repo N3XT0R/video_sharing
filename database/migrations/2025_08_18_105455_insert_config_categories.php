cc9fbcbc2fbd4fdcf61c9c0fc5d05d44d10a9ad1
Ilya Beliaev <info@php-dev.info>
2025-08-18 11:01:29 +0200

    changed for more usability.

---
+++ database/migrations/2025_08_18_105455_insert_config_categories.php
@@ -0,0 +1,40 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $timestamp = Carbon::now()->format('Y-m-d H:i:s');
+        $categories = collect(['default', 'oauth', 'email'])->map(fn($key) => [
+            'key' => $key,
+            'is_visible' => $key !== 'oauth',
+            'created_at' => $timestamp,
+            'updated_at' => $timestamp,
+        ])->all();
+        DB::table('config_categories')->insert($categories);
+
+        $oAuthId = DB::table('config_categories')->where('key', 'oauth')->value('id');
+        DB::table('configs')->where('key', 'dropbox_refresh_token')->update([
+            'config_category_id' => $oAuthId,
+        ]);
+
+        $emailId = DB::table('config_categories')->where('key', 'email')->value('id');
+        DB::table('configs')->whereIn('key', ['email_admin_mail', 'email_your_name'])->update([
+            'config_category_id' => $emailId,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
