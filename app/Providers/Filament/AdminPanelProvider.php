7b929d0c47483919c7a98cb0103d13aabb0320a8
Ilya Beliaev <info@php-dev.info>
2025-08-20 12:32:27 +0200

    added logviewer.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Boquizo\FilamentLogViewer\FilamentLogViewerPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -53,6 +54,11 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                FilamentLogViewerPlugin::make()
+                    ->navigationGroup('System')
+                    ->navigationLabel('Log Viewer'),
             ]);
     }
 }
