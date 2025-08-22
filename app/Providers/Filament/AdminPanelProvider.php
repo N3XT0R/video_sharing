3d2cdbef693e8cf6520f430d68df7e6f62bbbdc8
Ilya Beliaev <info@php-dev.info>
2025-08-22 20:56:23 +0200

    added mail log.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Tapp\FilamentMailLog\FilamentMailLogPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -60,6 +61,7 @@ class AdminPanelProvider extends PanelProvider
                 FilamentLogViewerPlugin::make()
                     ->navigationGroup('System')
                     ->navigationLabel('Log Viewer'),
+                FilamentMailLogPlugin::make(),
             ]);
     }
 }
---
+++ lang/vendor/filament-maillog/de/filament-maillog.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'navigation.group' => 'Protokolle',
+
+    'navigation.maillog.label' => 'E-Mail-Protokoll',
+    'navigation.maillog.plural-label' => 'E-Mail-Protokolle',
+
+    'table.heading' => 'E-Mail-Protokolle',
+
+    'column.status' => 'Status',
+    'column.subject' => 'Betreff',
+    'column.to' => 'An',
+    'column.from' => 'Von',
+    'column.cc' => 'CC',
+    'column.bcc' => 'BCC',
+    'column.message_id' => 'Message-ID',
+    'column.delivered' => 'Zugestellt',
+    'column.opened' => 'Geöffnet',
+    'column.bounced' => 'Unzustellbar',
+    'column.complaint' => 'Beschwerde',
+    'column.body' => 'Nachrichtentext',
+    'column.headers' => 'Kopfzeilen',
+    'column.attachments' => 'Anhänge',
+    'column.data' => 'Daten',
+    'column.created_at' => 'Erstellt am',
+    'column.updated_at' => 'Aktualisiert am',
+];
