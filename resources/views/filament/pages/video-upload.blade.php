986a560c97a4d4cf691adfef48aa87cb833c162c
Ilya Beliaev <info@php-dev.info>
2025-08-27 23:50:27 +0200

    filament upgrade tool removed.

--- resources/views/filament/pages/video-upload.blade.php
+++ resources/views/filament/pages/video-upload.blade.php
@@ -1,8 +1,8 @@
-<x-filament::page>
-    <form wire:submit.prevent="submit" class="space-y-6">
+<x-filament-panels::page>
+    <x-filament-panels::form wire:submit.prevent="submit" class="space-y-6">
         {{ $this->form }}
         <x-filament::button type="submit">
             Upload
         </x-filament::button>
-    </form>
-</x-filament::page>
+    </x-filament-panels::form>
+</x-filament-panels::page>
