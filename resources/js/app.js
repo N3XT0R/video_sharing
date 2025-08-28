581f1e59ea3f0392c7f1cacf8fbd55807fb1d8ba
Ilya Beliaev <N3XT0R@users.noreply.github.com>
2025-08-28 19:30:55 +0200

    Bundle noUiSlider via npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
+import noUiSlider from 'nouislider';
+import 'nouislider/dist/nouislider.css';
 import ZipDownloader from './components/ZipDownloader';
+
+window.noUiSlider = noUiSlider;
 
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('zipForm');
